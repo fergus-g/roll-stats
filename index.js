4ccbedf984447f9734759293318320790c5608aa
fergus-g <fergus.gildea@utrack.com>
2025-01-21 16:22:25 +0000

    Created standard array function and test cases

---
+++ index.js
@@ -0,0 +1,4 @@
+export function rollStandardArray() {
+  let standardArray = [15, 14, 13, 12, 10, 8];
+  return standardArray.sort(() => Math.random() - 0.5);
+}
---
+++ index.test.js
@@ -0,0 +1,15 @@
+import { test, expect } from "vitest";
+import { rollStandardArray, rollMethodI } from "./index.js";
+
+test("calling roll standard array returns a shuffled array", () => {
+  // Arrange
+  let standardArray = [15, 14, 13, 12, 10, 8];
+
+  // Act
+  let result = rollStandardArray();
+
+  // Assert
+  expect(result).toHaveLength(standardArray.length);
+  expect(result).toEqual(expect.arrayContaining(standardArray));
+  expect(result).not.toEqual(standardArray);
+});
